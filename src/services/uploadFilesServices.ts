25b272834608ae642fc8643dd5402d03aff9cb05
andres santacruz <andres_v10@live.com>
2022-11-21 10:28:09 -0500

    [FORMAT]: Change format

--- src/services/uploadFilesServices.ts
+++ src/services/uploadFilesServices.ts
@@ -1,18 +1,23 @@
-import fileUpload, {UploadedFile} from "express-fileupload";
-import  cloudinary  from "../config/cloudinary";
+import fileUpload, { UploadedFile } from "express-fileupload";
+import cloudinary from "../config/cloudinary";
 
 export const uploadFiles = async (files: fileUpload.FileArray) => {
   const resArr: string[] = [];
   const keyArray = Object.keys(files);
-  
+
+  console.log('fileee', files)
+
   for (let key of keyArray) {
     const path = (files[key] as UploadedFile).tempFilePath;
     try {
-      const resCloud = await cloudinary.uploader.upload(path, { folder: "subetuarchivo"});
+      const resCloud = await cloudinary.uploader.upload(path, {
+        resource_type: "raw",
+        folder: "subetuarchivo",
+      });
       resArr.concat(resCloud.url);
     } catch (error: any) {
-      console.log('ERRORRRRR-->', error)
-      console.log('my err -->', error.message)
+      console.log("ERRORRRRR-->", error);
+      console.log("my err -->", error.message);
     }
   }
 

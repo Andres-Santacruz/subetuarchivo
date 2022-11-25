7eeabe9f792ea33cf290e97bd09dc2aa7633fd36
andres santacruz <andres_v10@live.com>
2022-11-25 15:07:31 -0500

    [FIX]: Change metod upload and push urls

--- src/services/uploadFilesServices.ts
+++ src/services/uploadFilesServices.ts
@@ -7,19 +7,26 @@ export const uploadFiles = async (files: fileUpload.FileArray) => {
 
   console.log('fileee', files)
 
+
   for (let key of keyArray) {
-    const path = (files[key] as UploadedFile).tempFilePath;
+    const oneFile = files[key] as UploadedFile;
+    const path = `./temp/${Date.now()}__${oneFile.name}`;
+    await oneFile.mv(path);
+    // const ext = oneFile.name.split('.').at(-1);
     try {
       const resCloud = await cloudinary.uploader.upload(path, {
         resource_type: "raw",
         folder: "subetuarchivo",
+        use_filename: true
       });
-      resArr.concat(resCloud.url);
+      console.log('resCloud -->', resCloud)
+      resArr.push(resCloud.secure_url);
+      console.log('resArr ANTESSSSS', resArr)
     } catch (error: any) {
-      console.log("ERRORRRRR-->", error);
-      console.log("my err -->", error.message);
+      console.log("my err cloudi -->", error.message);
+      return []
     }
   }
-
+  console.log('resArr', resArr)
   return resArr;
 };

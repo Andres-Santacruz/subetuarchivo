18d228ddc93128ead2387fa4c8ef0392ae547f39
andres santacruz <andres_v10@live.com>
2022-11-30 11:55:13 -0500

    [FEATURE]: Add file deleted to server

--- src/services/uploadFilesServices.ts
+++ src/services/uploadFilesServices.ts
@@ -1,12 +1,17 @@
 import fileUpload, { UploadedFile } from "express-fileupload";
 import cloudinary from "../config/cloudinary";
+import {unlink} from "fs-extra";
+
+interface IFileInfo {
+  url: string;
+  public_id: string;
+}
 
 export const uploadFiles = async (files: fileUpload.FileArray) => {
-  const resArr: string[] = [];
+  const resArr: IFileInfo[] = [];
   const keyArray = Object.keys(files);
 
-  console.log('fileee', files)
-
+  console.log("fileee", files);
 
   for (let key of keyArray) {
     const oneFile = files[key] as UploadedFile;
@@ -17,16 +22,16 @@ export const uploadFiles = async (files: fileUpload.FileArray) => {
       const resCloud = await cloudinary.uploader.upload(path, {
         resource_type: "raw",
         folder: "subetuarchivo",
-        use_filename: true
+        use_filename: true,
       });
-      console.log('resCloud -->', resCloud)
-      resArr.push(resCloud.secure_url);
-      console.log('resArr ANTESSSSS', resArr)
+      console.log("resCloud -->", resCloud);
+      resArr.push({ url: resCloud.secure_url, public_id: resCloud.public_id });
+      await unlink(path);
     } catch (error: any) {
       console.log("my err cloudi -->", error.message);
-      return []
+      return [];
     }
   }
-  console.log('resArr', resArr)
+  console.log("resArr", resArr);
   return resArr;
 };
